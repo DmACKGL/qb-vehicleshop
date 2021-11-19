8027d85f6d12ad9c3ea1c5299731866885c377ff
Tanveer <70646347+wtftanveer@users.noreply.github.com>
2021-11-19 22:32:07 +0600

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -12,14 +12,7 @@ local provisoryObject = {}
 local rgbColorSelected = {255,255,255,}
 local rgbSecondaryColorSelected = {255,255,255,}
 
-QBCore = nil
-
-Citizen.CreateThread(function ()
-	while QBCore == nil do
-		TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-		Citizen.Wait(200)
-	end
-end)
+QBCore = exports['qb-core']:GetCoreObject()
 
 Citizen.CreateThread(function()
     while true do
